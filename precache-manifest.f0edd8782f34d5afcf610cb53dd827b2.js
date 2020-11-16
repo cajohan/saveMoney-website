4e1d0f2fc664c2aab00be5e9f7434d1438c7b4c6
huangdi <huangdi_c@163.com>
2020-11-16 09:27:21 +0800

    deploy

---
+++ precache-manifest.f0edd8782f34d5afcf610cb53dd827b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88c6b4e007600b8f723",
+    "url": "/saveMoney-website/css/app.9ea1803a.css"
+  },
+  {
+    "revision": "429a51f2e04580db1a385578915b8fd4",
+    "url": "/saveMoney-website/index.html"
+  },
+  {
+    "revision": "f88c6b4e007600b8f723",
+    "url": "/saveMoney-website/js/app.ec6c20b8.js"
+  },
+  {
+    "revision": "248b2f501e6574efaa59",
+    "url": "/saveMoney-website/js/chunk-vendors.fff02e63.js"
+  },
+  {
+    "revision": "f811448f486bfbe00611669585fd6057",
+    "url": "/saveMoney-website/manifest.json"
+  },
+  {
+    "revision": "b917b79b7f75f215d30ac1fac2f4cc10",
+    "url": "/saveMoney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/saveMoney-website/robots.txt"
+  }
+]);
